71aa62d5744d54ee5ca07493a7ddc4f0fb8fd17e
Lucia <2313970630@qq.com>
2021-03-26 18:42:52 +0800

    .

--- pages/api/rank.js
+++ pages/api/rank.js
@@ -4,6 +4,8 @@ const HEADERS = {
     'user-agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
     'origin': "https://i.y.qq.com",
     'referer': "https://i.y.qq.com/",
+    'Access-Control-Allow-Origin': '*',
+    "Content-Type": 'text/plain'
 }
 
 const request = require('request-promise');
