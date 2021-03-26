08bfb9a201b30e3281116c8ecf584f12847e0ac4
Lucia <2313970630@qq.com>
2021-03-26 19:47:11 +0800

    .

--- pages/api/rank.js
+++ pages/api/rank.js
@@ -1,18 +1,18 @@
-const cors = require('cors')
+const cors = require('cors');
+const express = require('express');
+const app = express()
 const HEADERS = {
     'authority': "u.y.qq.com",
     'accept': "application / json",
     'user-agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
     'origin': "https://i.y.qq.com",
-    'referer': "https://i.y.qq.com/",
-    'Access-Control-Allow-Origin': '*',
-    "Access-Control-Allow-Methods": "GET, POST",
-    "Content-Type": 'text/plain'
+    'referer': "https://i.y.qq.com/"
 }
+app.use(cors())
 
 const request = require('request-promise');
 
-export default cors(async(req, res) => {
+export default async(req, res) => {
     const url = `https://u.y.qq.com/cgi-bin/musicu.fcg?_=${+new Date()}&data={"comm":{"g_tk":1775699468,"uin":2313970630,"format":"json","inCharset":"utf-8","outCharset":"utf-8","notice":0,"platform":"h5","needNewCode":1,"ct":23,"cv":0},"topList":{"module":"musicToplist.ToplistInfoServer","method":"GetAll","param":{}}}`
     try {
         res.json(await request({
@@ -23,4 +23,4 @@ export default cors(async(req, res) => {
     } catch (e) {
         res.json({ error: e.massage })
     }
-})
+}
